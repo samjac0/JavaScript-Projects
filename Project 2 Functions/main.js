5061afbc0b08b54f742e526ae21b0f9c0b81de2c
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-03-04 11:18:30 -0800

    step 69

---
+++ Project 2 Functions/main.js
@@ -0,0 +1,14 @@
+window.alert("This is a test!") // popup
+var A = "This is a string"; // assigning variables
+document.write(A);
+document.write(3 + 3); 
+function My_first_function() { //first function
+    var str = "This is the button text!"; 
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot to learn from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
