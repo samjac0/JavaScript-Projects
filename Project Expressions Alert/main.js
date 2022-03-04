e2f159d0b8546a31d90d84c8447c80f76c5b06f1
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-03-04 10:36:16 -0800

    step 64

---
+++ Project Expressions Alert/main.js
@@ -0,0 +1,8 @@
+window.alert("This is a test!") // popup
+var A = "This is a string"; // assigning variables
+document.write(A);
+document.write(3 + 3); 
+function My_first_function() { //first function
+    var str = "This is the button text!"; 
+    document.getElementById("Button_Text").innerHTML = str;
+}
