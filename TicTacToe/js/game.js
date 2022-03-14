1fe93fe3230f851dd0ec221def8bca22f8721457
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-03-14 15:00:38 -0700

    errors

--- TicTacToe/js/game.js
+++ TicTacToe/js/game.js
@@ -1,12 +1,12 @@
 let activePlayer = "X";
 let selectedSquares = [];
 function placeXOrO(squareNumber) {
-    if (selectedSquares.some(element => element.includes(squareNumber))) {
-        let select = document.getElementById(suareNumber);
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        let select = document.getElementById(squareNumber);
         if (activePlayer === "X") {
             select.style.backgroundImage = "url('images/x.png')"
         } else {
-            select.style.backgroundImage = "url('images/o.png')"
+            select.style.backgroundImage = "url('images/o.jpg')"
         }
         selectedSquares.push(squareNumber + activePlayer);
         checkWinConditions();
@@ -41,7 +41,7 @@ function placeXOrO(squareNumber) {
 }
 
 function checkWinConditions() {
-    if (arrayIncludes("0X", "1X", "2X")) { drawWinLine(50, 100, 558, 100) }
+    if      (arrayIncludes("0X", "1X", "2X")) { drawWinLine(50, 100, 558, 100) }
     else if (arrayIncludes("3X", "4X", "5X")) { drawWinLine(50, 304, 558, 304) }
     else if (arrayIncludes("6X", "7X", "8X")) { drawWinLine(50, 508, 558, 508) }
     else if (arrayIncludes("0X", "3X", "6X")) { drawWinLine(100, 50, 100, 558) }
@@ -70,4 +70,67 @@ function checkWinConditions() {
         const c = selectedSquares.includes(squareC)
         if (a === true && b === true && c === true) { return true }
     }
+}
+
+function disableClick() {
+    body.style.pointerEvents = "none";
+    setTimeout(function () { body.style.pointerEvents = "auto"; }, 1000);
+}
+
+function audio(audioURL) {
+    let audio = new Audio(audioURL);
+    audio.play();
+}
+
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    const canvas = document.getElementById("win-lines")
+    const c = canvas.getContext("2d");
+    let x1 = coordX1,
+        y1 = coordY1,
+        x2 = coordX2,
+        y2 = coordY2,
+        x = x1,
+        y = y1;
+    
+    
+    function animateLineDrawing() {
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        c.clearRect(0, 0, 608, 608)
+        c.beginPath();
+        c.moveTo(x1, y1)
+        c.lineTo(x, y)
+        c.lineWidth = 10;
+        c.strokeStyle = "rgba(70, 255, 33, .8";
+        c.stroke();
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) { y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+
+        }
+        // for 6 4 2 wins
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+    function clear() {
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+    
+    disableClick();
+    audio("./media/win.wav");
+    animateLineDrawing();
+    setTimeout(function () { clear(); resetGame(); }, 1000);
+}
+
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i))
+        square.style.backgroundImage = ""
+    }
+    selectedSquares = [];
 }
